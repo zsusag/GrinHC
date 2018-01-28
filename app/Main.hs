76491af59a16b653a7f980713444502731b0ea09
Zachary J. Susag <susagzac@grinnell.edu>
2018-01-27 20:10:56 -0600

    Add capabilities of bare bones project

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
 module Main where
 
+import Data.Semigroup((<>))
+import Options.Applicative
+
 import Lib
 
+
+data Args = Args
+  { len       :: Bool
+  , otherArgs :: [String] }
+
+parseLen :: Parser Bool
+parseLen = switch
+      (  long "length"
+        <> short 'l'
+        <> help "print length of arguments instead of arguments themselves")
+
+parseOtherArgs :: Parser [String]
+parseOtherArgs = many (argument str (metavar "ARGS..."))
+
+parseArgs :: Parser Args
+parseArgs = Args <$> parseLen <*> parseOtherArgs
+
+parseArgInfo :: ParserInfo Args
+parseArgInfo = info (parseArgs <**> helper)
+  (  fullDesc
+  <> progDesc "A compiler written in Haskell for CSC-312 during 2018S"
+  <> header   "GrinHC: A compiler written in Haskell at Grinnell College" )
+
 main :: IO ()
-main = helloWorld
+main = do
+  options <- execParser parseArgInfo
+  case options of
+    Args True xs  -> mapM_ (putStrLn . show . length) xs
+    Args False xs -> mapM_ putStrLn xs
