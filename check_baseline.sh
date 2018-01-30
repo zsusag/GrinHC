6f9dd0eb8a39d780499bf6a4a0ece59c23ee8876
Zachary J. Susag <susagzac@grinnell.edu>
2018-01-29 21:50:09 -0600

    Use mktemp instead of making my own tmp file

--- check_baseline.sh
+++ check_baseline.sh
@@ -8,15 +8,21 @@ fi
 
 TEST_DIR="$(realpath $1)"
 
+
 if [ -d $TEST_DIR ] && [ "$(basename $TEST_DIR)" == "assignment_01" ]
 then
     for file in $TEST_DIR/*.test; do
         echo "Checking $file..."
+        # Extract command from file.
         command="$(cat $file)"
-        tmpfile="$file.tmp"
+
+        # Create a tmp file within /tmp
+        tmpfile=$(mktemp)
+
+        # Evaluate and diff the output
         eval $command > $tmpfile
         outfile="${file%.*}.out"
-        diff $outfile $tmpfile
+        diff $tmpfile $outfile
         rm $tmpfile
     done
     echo "Done testing in $TEST_DIR."
