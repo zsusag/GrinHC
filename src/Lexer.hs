ba545d42238e9959c5239e5117c6152ebaf3d18a
Zachary J. Susag <susagzac@grinnell.edu>
2018-02-04 19:20:43 -0600

    Implement basic lexing for integer addition programs

---
+++ src/Lexer.hs
@@ -0,0 +1,27 @@
+module Lexer where
+
+import Data.Char
+
+import Token
+
+lexString :: String -> IO [Token]
+lexString s = return $ reverse $ lexStringH s []
+
+lexStringH :: String -> [Token] -> [Token]
+lexStringH ('(':xs) ts = lexStringH xs (TokenLParen:ts)
+lexStringH (')':xs) ts = lexStringH xs (TokenRParen:ts)
+lexStringH ('+':xs) ts = lexStringH xs (TokenPlus:ts)
+lexStringH (x:xs) ts
+  | isDigit x =
+    let tokenInt = reverse $ lexInt xs [x] in
+      lexStringH (drop (length tokenInt - 1) xs)
+      (TokenInt (read tokenInt):ts)
+  | isSpace x = lexStringH xs ts
+  | otherwise =  error ("Invalid character: " ++ [x])
+lexStringH [] ts = ts
+
+lexInt :: String -> String -> String
+lexInt (x:xs) digits = if isDigit x
+                       then lexInt xs (x:digits)
+                       else digits
+lexInt [] digits = digits
