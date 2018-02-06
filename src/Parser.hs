337d30f8364cb2c1ce1e50ef31a44ae64ec1dc38
Zachary J. Susag <susagzac@grinnell.edu>
2018-02-05 22:11:16 -0600

    Add ability to parse boolean s-expressions

--- src/Parser.hs
+++ src/Parser.hs
@@ -3,25 +3,32 @@ module Parser where
 import Token
 import AST
 
-parseTokenStream :: [Token] -> IO (Exp Int)
+parseTokenStream :: [Token] -> IO Exp
 parseTokenStream ts = let (ast,ts') = parseTokenStreamH ts
                       in case ts' of
                            [] -> return ast
                            _  -> error ("Error during parsing. Unable to parse " ++ show ts')
 
 
-parseTokenStreamH :: [Token] -> (Exp Int, [Token])
+parseTokenStreamH :: [Token] -> (Exp, [Token])
+parseTokenStreamH (TokenLParen:TokenIf:ts) = let (e1, ts') = parseTokenStreamH ts
+                                                 (e2, ts'') = parseTokenStreamH ts'
+                                                 (e3, ts''') = parseTokenStreamH ts''
+  in case ts''' of
+       (TokenRParen:ts'''') -> (EIf e1 e2 e3, ts'''')
+       _                    -> error "Parenthesis mismatch"
 parseTokenStreamH (TokenLParen:ts) = let (e1,ts') = parseTokenStreamH $ tail ts
                                          (e2,ts'') = parseTokenStreamH ts'
   in case ts'' of
        (TokenRParen:ts''') -> case head ts of
-                                TokenPlus -> (Add e1 e2, ts''')
-                                TokenSub  -> (Sub e1 e2, ts''')
-                                TokenMult -> (Mul e1 e2, ts''')
-                                TokenDiv  -> (Div e1 e2, ts''')
+                                TokenPlus -> (EAdd e1 e2, ts''')
+                                TokenSub  -> (ESub e1 e2, ts''')
+                                TokenMult -> (EMul e1 e2, ts''')
+                                TokenDiv  -> (EDiv e1 e2, ts''')
+                                TokenLEQ  -> (ELeq e1 e2, ts''')
                                 t         -> error ("Operator not supported: " ++ show t)
        _                   -> error "Parentheses mismatch."
-parseTokenStreamH (TokenInt x:ts) = (LitInt x, ts)
-parseTokenStreamH _ = error "Improper syntax."
-
-
+parseTokenStreamH (TokenInt x:ts) = (EInt x, ts)
+parseTokenStreamH (TokenBool True:ts) = (EBool True, ts)
+parseTokenStreamH (TokenBool False:ts) = (EBool False, ts)
+parseTokenStreamH ts = error ("Improper syntax: " ++ show ts)
