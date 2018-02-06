e075f0af14ea7d26f359a5fd6cf43702a4d80c0c
Zachary J. Susag <susagzac@grinnell.edu>
2018-02-05 22:10:50 -0600

    Remove GADTs as typing was too strong for this parser and move to basic algebraic data type

--- src/AST.hs
+++ src/AST.hs
@@ -1,25 +1,68 @@
-{-# LANGUAGE GADTs #-}
 module AST where
 
-data Exp a where
-  LitInt :: Int -> Exp Int
-  Add    :: Exp Int -> Exp Int -> Exp Int
-  Sub    :: Exp Int -> Exp Int -> Exp Int
-  Mul    :: Exp Int -> Exp Int -> Exp Int
-  Div    :: Exp Int -> Exp Int -> Exp Int
-  
-instance Show a => Show (Exp a) where
-  show (LitInt x) = show x
-  show (Add x y)  = show x ++ " + " ++ show y
-  show (Sub x y)  = show x ++ " - " ++ show y
-  show (Mul x y)  = show x ++ " * " ++ show y
-  show (Div x y)  = show x ++ " / " ++ show y
+data Exp = EInt Int
+  | EAdd Exp Exp
+  | ESub Exp Exp
+  | EMul Exp Exp
+  | EDiv Exp Exp
+  | EBool Bool
+  | ELeq Exp Exp
+  | EIf Exp Exp Exp
 
-eval :: Exp a -> a
-eval (LitInt n) = n
-eval (Add e1 e2) = eval e1 + eval e2
-eval (Sub e1 e2) = eval e1 - eval e2
-eval (Mul e1 e2) = eval e1 * eval e2
-eval (Div e1 e2) = if eval e2 == 0
-                   then error "Cannot divide by 0"
-                   else eval e1 `div` eval e2
+instance Show Exp where
+  show (EInt n)       = show n
+  show (EAdd e1 e2)   = show e1 ++ " + " ++ show e2
+  show (ESub e1 e2)   = show e1 ++ " - " ++ show e2
+  show (EMul e1 e2)   = show e1 ++ " * " ++ show e2
+  show (EDiv e1 e2)   = show e1 ++ " / " ++ show e2
+  show (EBool b)      = show b
+  show (ELeq e1 e2)   = show e1 ++ " <= " ++ show e2
+  show (EIf e1 e2 e3) = "if " ++ show e1 ++ " then " ++ show e2
+                        ++ " else " ++ show e3
+evaluate :: Exp -> String
+evaluate e = let simp = simplify e
+             in case simp of
+                  EInt n  -> show n
+                  EBool b -> show b
+                  _       -> error "Incomplete evaluation: Unable to evaluate S-Expression"
+
+simplify :: Exp -> Exp
+simplify (EAdd e1 e2) = let e1' = simplify e1
+                            e2' = simplify e2
+  in case (e1',e2') of
+       (EInt n1, EInt n2) -> EInt $ n1 + n2
+       _                  -> error ("Type Error: \"" ++ show (EAdd e1' e2') ++
+                                    "\" does not typecheck.")
+simplify (ESub e1 e2) = let e1' = simplify e1
+                            e2' = simplify e2
+  in case (e1',e2') of
+       (EInt n1, EInt n2) -> EInt $ n1 - n2
+       _                  -> error ("Type Error: \"" ++ show (ESub e1' e2') ++
+                             "\" does not typecheck.")
+simplify (EMul e1 e2) = let e1' = simplify e1
+                            e2' = simplify e2
+  in case (e1',e2') of
+       (EInt n1, EInt n2) -> EInt $ n1 * n2
+       _                  -> error ("Type Error: \"" ++ show (EMul e1' e2') ++
+                             "\" does not typecheck.")
+simplify (EDiv e1 e2) = let e1' = simplify e1
+                            e2' = simplify e2
+  in case (e1',e2') of
+       (EInt n1, EInt n2) -> if n2 /= 0
+                             then EInt $ n1 `div` n2
+                             else error "Divide by Zero: Cannot divide by zero"
+       _                  -> error ("Type Error: \"" ++ show (EDiv e1' e2') ++
+                             "\" does not typecheck.")
+simplify (ELeq e1 e2) = let e1' = simplify e1
+                            e2' = simplify e2
+  in case (e1',e2') of
+       (EInt n1, EInt n2) -> EBool $ n1 <= n2
+       _                  -> error ("Type Error: \"" ++ show (ELeq e1' e2') ++
+                             "\" does not typecheck.")
+simplify (EIf e1 e2 e3) = if b1 then simplify e2 else simplify e3
+  where b1 = let e1' = simplify e1
+             in case e1' of
+                  (EBool b) -> b
+                  _         -> error ("Type Error: " ++ show e1' ++
+                                      " is not a boolean value within an \"if\" statement")
+simplify e = e
