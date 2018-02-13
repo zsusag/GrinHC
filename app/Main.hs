619b2c07d5ee00cfbc89c446ce42a78e2dccef35
Zachary J. Susag <susagzac@grinnell.edu>
2018-02-12 18:41:40 -0600

    Allow lex and parse options to be used to print out token steam and ast respectively

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,40 @@
+{-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 module Main where
 
 import Data.Semigroup((<>))
 import Options.Applicative
+import Control.Monad
 import System.IO
 import System.Directory
+import System.Exit
 
 import Lexer
 import Parser
 import Eval
 
-newtype Args = Args
-  { file      :: FilePath }
+data Args = Args
+  { file :: FilePath
+  , printTS :: Bool
+  , printAST :: Bool
+  }
 
-parseLen :: Parser Bool
-parseLen = switch
-      (  long "length"
-        <> short 'l'
-        <> help "Print length of arguments instead of arguments themselves")
+parseLex :: Parser Bool
+parseLex = switch
+  (long "lex"
+   <> short 'l'
+   <> help "Print the token stream instead of evaluating code")
+
+parseParse :: Parser Bool
+parseParse = switch
+  (long "parse"
+  <> short 'p'
+  <> help "Print the AST instead of evaluating code")
 
 parseFilePath :: Parser FilePath
 parseFilePath = argument str (metavar "FILE")
 
 parseArgs :: Parser Args
-parseArgs = Args <$> parseFilePath
+parseArgs = Args <$> parseFilePath <*> parseLex <*> parseParse
 
 parseArgInfo :: ParserInfo Args
 parseArgInfo = info (parseArgs <**> helper)
@@ -32,21 +44,24 @@ parseArgInfo = info (parseArgs <**> helper)
 
 main :: IO ()
 main = do
-  options <- execParser parseArgInfo
-  case options of
-    Args filePath -> do
-      handle <- tryOpen filePath
-      contents <- hGetContents handle
-      let tokenStream = alexScanTokens contents
---      print tokenStream
-      let ast = parse tokenStream
---      print ast
-      putStrLn $ evaluate ast
-      hClose handle
-    where tryOpen filePath = case filePath of
-            [] -> errorWithoutStackTrace ("Could not open file: File does not exist: " ++ filePath)
-            _  -> do
-              exists <- doesFileExist filePath
-              if exists
-                then openFile filePath ReadMode
-                else errorWithoutStackTrace ("Could not open file: File does not exist: " ++ filePath)
+   (Args file printTS printAST) <- execParser parseArgInfo
+   handle <- tryOpen file
+   contents <- hGetContents handle
+   let tokenStream = alexScanTokens contents
+   printAndExit printTS tokenStream handle
+   let ast = Parser.parse tokenStream
+   printAndExit printAST ast handle
+   putStrLn $ evaluate ast
+   hClose handle
+     where
+       tryOpen filePath = case filePath of
+         [] -> errorWithoutStackTrace ("Could not open file: File does not exist: " ++ filePath)
+         _  -> do
+           exists <- doesFileExist filePath
+           if exists
+             then openFile filePath ReadMode
+             else errorWithoutStackTrace ("Could not open file: File does not exist: " ++ filePath)
+       printAndExit b ts handle = when b (do print ts
+                                             hClose handle
+                                             exitWith (ExitSuccess))
+    
