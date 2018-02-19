a4422547aa81ce4012430bdbf9691e3cb8823649
Zachary J. Susag <susagzac@grinnell.edu>
2018-02-18 18:55:56 -0600

    Create interface for providing errors with position information

---
+++ src/Error.hs
@@ -0,0 +1,8 @@
+module Error where
+
+import Lang (Pos)
+
+posError :: Pos -> String -> String -> a
+posError (line,col) errortype msg = errorWithoutStackTrace posMsg
+  where
+    posMsg = errortype ++ " at (line " ++ show line ++ ", column " ++ show col ++ ")" ++ msg
